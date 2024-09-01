516fab496e5d5fabac7d701c41fc5b20024a76bd
Sameer42069 <sameerbarodawala2014@gmail.com>
2024-09-01 08:14:59 +0530

    main_code.py

--- main_code.py
+++ main_code.py
@@ -1,16 +1,19 @@
-import flask
-from flask import request,Flask, render_template
-main_code=Flask(__name__)
-@main_code.route('/')
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
 def web():
     return render_template('index.html')
-@main_code.route('/submit', methods=['POST'])
+
+@app.route('/submit', methods=['POST'])
 def submit():
     name = request.form['name']
     user_ip = request.remote_addr
     print(f"Name entered: {name}")
-    print(f"IP address{user_ip}")
+    print(f"IP address: {user_ip}")
     return f"Hello, {name}!"
 
 if __name__ == '__main__':
-    main_code.run(debug=True)
+    app.run(debug=True)
+
